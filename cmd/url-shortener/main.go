c7dccc04ca4eb89878f3ebda91c6f5b4c5bab5de
ilya.aleksandrov <ilya.aleksandrov@travelline.ru>
2025-12-08 17:41:27 +0300

    Создан логгер для разных сред (local, qa, prod) и slient логер, который можно использовать для тестирования

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envQa    = "qa"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+	log = log.With(slog.String("env", cfg.Env))
+
+	log.Info("initializing server", slog.String("address", cfg.Address)) // Помимо сообщения выведем параметр с адресом
+	log.Debug("logger debug mode enabled")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envQa:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
